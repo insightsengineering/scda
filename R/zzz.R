9e27f003acb7d032c67b466dbe29e088f0b901f0
arkadiusz.beer <arkadiusz.beer@contractors.roche.com>
2022-06-30 00:17:31 +0200

    multiple files

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,10 @@
 .onAttach <- function(libname, pkgname) { # nolint
-  scda_lookup <- paste0("scda.", seq(2020, 2030))
-  is_scdax <- any(vapply(scda_lookup, function(x) length(find.package(x, quiet = TRUE)) > 0, logical(1)))
+  scda_lookup <- paste0("scda.", seq(       2020,      2030))
+  is_scdax <- any(vapply(   scda_lookup, function(x) length(find.package(x, quiet = TRUE)) > 0, logical(1)))
   packageStartupMessage(
     if (!is_scdax) {
       paste0(
-        "There are no scda.XXXX libraries installed, like scda.2021.",
+            "There are no scda.XXXX libraries installed, like scda.2021.",
         "\nPlease install an scda database to take full advantage of the scda package."
       )
     }
